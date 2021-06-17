6e74e6b92536a83d96b0e8cced1b302ddf1bb707
MoisesEnrique <moises.mac@gmail.com>
2021-06-16 19:23:24 -0500

    Editar la pagina de inicio del proyecto

--- projects/calc/views.py
+++ projects/calc/views.py
@@ -1,3 +1,10 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+# Creamos la funcion home, para que funcione con la creada en urls.py.
+def home(request):   #recibira la solicitud del cliente
+    return HttpResponse("Hello World")
