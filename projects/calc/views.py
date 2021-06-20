cda1a12ab9069d3c4556b36a1489315a6b50eba0
MoisesEnrique <moises.mac@gmail.com>
2021-06-20 13:55:29 -0500

    Creando formulario para la suma de 2 numeros

--- projects/calc/views.py
+++ projects/calc/views.py
@@ -1,10 +1,18 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, request
 
 # Create your views here.
 
 # Creamos la funcion home, para que funcione con la creada en urls.py.
 def home(request):   #recibira la solicitud del cliente
-    return render(request, 'home.html',{'name':'Ricardo'})
+    return render(request, 'home.html', {'name':'Ricardo'})
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+
+    res = val1 + val2
+
+    return render(request, "result.html", {'result':res})
